2d4ce5950aa1a1f1598d5a37e8ac75cf72eb789b
Niravcodez <84302842+Niravcodez@users.noreply.github.com>
2022-02-10 17:38:30 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 song1 = "";
 song2 = "";
+
 leftWristX = 0;
 leftWristY = 0;
+
 rightWristX = 0;
 rightWristY = 0;
+
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+
+song1_status = "";
+song2_status = "";
 
 function preload()
 {
     song1 = loadSound("music.mp3");
     song2 = loadSound("music2.mp3");
 }
+
 function setup()
 {
     canvas = createCanvas(600, 500);
@@ -25,6 +34,23 @@ function setup()
 function draw()
 {
     image(video, 0, 0, 600, 500);
+
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+    
+    fill("red");
+    stroke("red");
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY, 20);
+        song1.stop();
+        if(song2_status == false)
+        {
+            song2.play();
+            document.getElementById("songname").innerHTML = "Playing That's What I Want by Lil Nas X";
+        }
+    }
 }
 
 function gotPoses(results)
@@ -39,6 +65,8 @@ function gotPoses(results)
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("righttWristX = " + rightWristX + " rightWristY = " + rightWristY);
+
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
     }
 }
 
